94203cc6e157b1064cbe3113018bbf3aa22e09a6
Mathangi-chandrasekar <mathu95win@gmail.com>
2025-04-09 18:06:44 +0100

    concept implemented

---
+++ src/OOPS_Project/Destination.java
@@ -0,0 +1,24 @@
+package airline;
+public enum Destination {
+    IRELAND(0, "IRL000"),
+    INDIA(40, "IND123"),
+    USA(30, "USA456"),
+    UK(20, "UKF246"),
+    CHINA(50, "CHN867");
+
+    private final int pricePerPerson;
+    private final String flightNumber;
+
+    Destination(int pricePerPerson, String flightNumber) {
+        this.pricePerPerson = pricePerPerson;
+        this.flightNumber = flightNumber;
+    }
+
+    public int getPricePerPerson() {
+        return pricePerPerson;
+    }
+
+    public String getFlightNumber() {
+        return flightNumber;
+    }
+}
